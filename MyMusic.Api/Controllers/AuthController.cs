896236e156b6d5980cae95586f471493c6230708
Aiden Arnkels-Webb <aiden@aidenwebb.com>
2022-10-10 08:42:08 +0100

    Wire up auth in API

---
+++ MyMusic.Api/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using MyMusic.Api.Resources;
+using MyMusic.Api.Validations;
+using MyMusic.Core.Models;
+using MyMusic.Core.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using MyMusic.Core.Models.Auth;
+
+namespace MyMusic.Api.Controllers
+{
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+        
+        public AuthController(IMapper mapper, UserManager<User> userManager){
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp([FromBody] UserSignUpResource userSignUpResource){
+            var user = _mapper.Map<UserSignUpResource, User>(userSignUpResource);
+
+            var userCreateResult = await _userManager.CreateAsync(user, userSignUpResource.Password);
+
+            if(!userCreateResult.Succeeded){
+                return BadRequest(userCreateResult.Errors);
+            }
+
+            return Created(string.Empty, string.Empty);
+        }
+
+        [HttpPost("signin")]
+        public async Task<IActionResult> SignIn(UserLoginResource userLoginResource){
+            //var user = await _userManager.Users.SingleOrDefaultAsync(u => u.Username == userLoginResource.Email);
+
+            var user = _userManager.Users.SingleOrDefault(u => u.UserName == userLoginResource.Email);
+
+            if(user == null){
+                return BadRequest("Invalid login attempt");
+            }
+
+            var userSignInResult = await _userManager.CheckPasswordAsync(user, userLoginResource.Password);
+
+            if(!userSignInResult){
+                return BadRequest("Invalid login attempt");
+            }
+
+            return Ok("User logged in successfully");
+        }
+
+
+
+    }
+}
